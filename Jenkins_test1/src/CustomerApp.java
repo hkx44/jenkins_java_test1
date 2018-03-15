d16d381f5ab8642f8ae2fa9a6dc5fbf9608fe5f5
ksiezakh <ksiezakh@RWAMW37787.emea.roche.com>
2018-03-15 17:29:37 +0100

    test

--- Jenkins_test1/src/CustomerApp.java
+++ Jenkins_test1/src/CustomerApp.java
@@ -1,9 +1,9 @@
 
-public class Customer {
+public class CustomerApp {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		System.out.print("CustomerApp works OK!");
+			
 	}
 
 }
